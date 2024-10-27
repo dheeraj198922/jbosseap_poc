0989388e56536100811be68ba0b0643a93fc0394
dheeraj198922 <81927269+dheeraj198922@users.noreply.github.com>
2024-10-28 02:24:33 +0530

    Add files via upload
    poc for logging with jboss eap 7.2 server

---
+++ src/main/java/com/jbosseap/jbosseap/JbosseapApplication.java
@@ -0,0 +1,13 @@
+package com.jbosseap.jbosseap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbosseapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbosseapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbosseap/jbosseap/JbosseapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbosseap.jbosseap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbosseapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
